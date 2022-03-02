89a0231664a12d326b53ef2242246ff74bea3189
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-02 14:07:32 +0000

    Initial Commit

---
+++ DesignCodeBootCamp/DesignCodeBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeBootCampApp.swift
+//  DesignCodeBootCamp
+//
+//  Created by Kyungyun Lee on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
